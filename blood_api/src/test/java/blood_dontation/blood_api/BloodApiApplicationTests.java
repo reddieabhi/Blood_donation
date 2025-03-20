d28cb28787259c98babc558c50ee69ff5f115c1d
Abhinay Reddy Bellamkonda <abhinayreddybellamkonda@Abhinays-MacBook-Air.local>
2025-03-20 22:47:50 +0530

    Added blood API code

---
+++ blood_api/src/test/java/blood_dontation/blood_api/BloodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package blood_dontation.blood_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
